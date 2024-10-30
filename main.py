e9806030f018adf4e724347ea02f323056eb01be
ellallalli <elallalivisuals@gmail.com>
2024-10-31 00:56:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from nicegui import ui
 import nicegui as ng
 
+
 @ng.ui.page('/')
 def main():
     ui.colors(accent='#6AD4DD')
@@ -10,7 +11,16 @@ def main():
     with ui.page_sticky(x_offset=650, y_offset=600):
         ui.button("est ce que je suis conne ?",on_click=lambda: ui.notify('Also Yes'), icon='help', color="blue" )
 
-ng.ui.run(host='0.0.0.0', port=443, title="Rim") 
+    
+
+    
+ng.ui.run(
+    host='0.0.0.0',
+    port=443,
+    title="Rim",
+    favicon='favicon.ico',
+   dark=True
+)
 
 
 
