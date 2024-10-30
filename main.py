3c2bc11ae6f194fb46fd4cdff6161bf8bc83da5e
ellallalli <elallalivisuals@gmail.com>
2024-10-31 00:24:11 +0100

    new main.py

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import nicegui as ng
 
 @ng.ui.page('/')
 def main():
-    ui.button("est ce qu'il m'aime ?",on_click=lambda: ui.notify('Yes') )
+    ui.colors(accent='#6AD4DD')
+    with ui.page_sticky(x_offset=670, y_offset=700):
 
-    ui.button("est ce que je suis conne ?",on_click=lambda: ui.notify('Also Yes') )
+        ui.button("est ce qu'il m'aime ?", icon='favorite' ,on_click=lambda: ui.notify('Yes'),color="red"  )
+    with ui.page_sticky(x_offset=650, y_offset=600):
+        ui.button("est ce que je suis conne ?",on_click=lambda: ui.notify('Also Yes'), icon='help', color="blue" )
 
 ng.ui.run(host='0.0.0.0', port=443, title="Rim") 
 
